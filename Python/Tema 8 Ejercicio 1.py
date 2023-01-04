c56d8a539b7ad1bfac25dc429e1c17ac25268232
Santiago <santiagoferval@gmail.com>
2023-01-04 15:45:34 +0100

    ejercicios tema 8

---
+++ Python/Tema 8 Ejercicio 1.py
@@ -0,0 +1,11 @@
+"""
+·En este ejercicio, tendréis que crear un archivo py donde creéis
+ un archivo txt, lo abráis y escribáis dentro del archivo. Para ello,
+ tendréis que acceder dos veces al archivo creado.
+"""
+
+import os
+from pathlib import Path
+from os import system
+
+ruta = Path("C:\Users\SFV Mecanizados\Desktop")
