3977d1752f3f826f33eec87272804ca7aaa847eb
Santiago <santiagoferval@gmail.com>
2023-01-04 11:20:59 +0100

    añado ejercicio 5 resuelto

--- Python/Ejercicio 5.py
+++ Python/Ejercicio 5.py
@@ -2,3 +2,14 @@
 ·Escribe una función que pueda decirte si un año (número entero) es
  bisiesto o no.
 """
+
+def verifica_año_bisiesto(year):
+
+    if year not in range(1, 9999):
+        print("No es un año valido, tiene que estar entre el 1 y el 9999.")
+    elif (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
+        print(f"El año {year} si es bisiesto.")
+    else:
+        print(f"El año {year} no es bisiesto.")
+
+verifica_año_bisiesto(int(input("Introduzca año a verificar: ")))
